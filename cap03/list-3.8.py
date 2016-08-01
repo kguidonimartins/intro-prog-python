f9c560226f6f1fc006763ddc02833cbcd9b5b6ce
Karlo Guidoni Martins <karlogregorio@gmail.com>
2016-08-01 20:25:38 -0300

    trabalhando com strings

---
+++ cap03/list-3.8.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Manipulação de strings no interpretador
+a = "ABCDEF"
+
+print(a[0])
+
+print(a[1])
+
+print(a[5])
+
+print(a[6])
+
+print(len(a))
---
+++ cap03/list-3.9.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Exemplo de concatenação
+
+s = "ABC"
+
+print(s + "C")
+
+print(s + "D" * 4)
+
+print("X" + "-" * 10 + "X")3
+
+print(s + "x4 = " + s * 4)
