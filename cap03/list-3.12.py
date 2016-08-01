89da7fbf1249aaa907a1da0dad4b51d3e83228f4
Karlo Guidoni Martins <karlogregorio@gmail.com>
2016-08-01 20:52:52 -0300

    composição de strings com números inteiros e decimais

---
+++ cap03/list-3.12.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Exemplo de composição de string com 
+#números inteiros e decimais
+
+nome = "João"
+
+idade = 22
+
+grana = 51.34
+
+print("%s tem %d anos e R$%f no bolso." % (nome, idade, grana))
+
+print("%s tem %d anos e R$%2.2f no bolso." % (nome, idade, grana))
+
+print("%s tem %d anos e R$%5.2f no bolso." % (nome, idade, grana))
+
+print("%12s tem %3d anos e R$%2.2f no bolso." % (nome, idade, grana))
+
+print("%24s tem %03d anos e R$%2.2f no bolso." % (nome, idade, grana))
+
+print("%-12s tem %-3d anos e R$%-5.2f no bolso." % (nome, idade, grana))
+
