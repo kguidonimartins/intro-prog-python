a15c182f1536ad0218769be13a3cd99df1821653
Karlo Guidoni Martins <karlogregorio@gmail.com>
2016-08-01 20:42:28 -0300

    composição com números inteiros e decimais

---
+++ cap03/list-3.10.py
@@ -0,0 +1,23 @@
+# Exemplos de composição com números inteiros
+
+idade = 22
+
+print("[%d]" % idade)
+
+print("[%03d]" % idade)
+
+print("[%010d]" % idade)
+
+print("[%20d]" % idade)
+
+print("[%020d]" % idade)
+
+print("[%-020d]" % idade)
+
+print("[%-20d]" % idade)
+
+print("[%3d]" % idade)
+
+print("[%100d]" % idade)
+
+print("[%-3d]" % idade)
---
+++ cap03/list-3.11.py
@@ -0,0 +1,7 @@
+# Exemplos de composição com números decimais
+
+print("%5f" % 5)
+
+print("%5.2f" % 5)
+
+print("%10.2f" % 5)
