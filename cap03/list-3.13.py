1589244418f8fe3f25b09008642572c05f3c3602
Karlo Guidoni Martins <karlogregorio@gmail.com>
2016-08-01 21:06:45 -0300

    fatiamento com índices

---
+++ cap03/list-3.13.py
@@ -0,0 +1,18 @@
+# Exemplo de fatiamento
+
+s = "ABCDEFGHI"
+
+print(s[0:2])
+
+print(s[1:2]) #?
+
+print(s[1]) #?
+
+# O primeiro índice à esquerda dos dois pontos indica o início da fatia e 
+#o segundo índice, o final.
+# O índice que indica o final da fatia não é incluído.
+
+print(s[:4]) # início implícito
+
+print(s[4:]) # final implícito
+
---
+++ cap03/list-3.14.py
@@ -0,0 +1,13 @@
+# Exemplos de fatiamento com valores negativos.
+# -1 indica o último caracter.
+# -2 indica o penúltimo caracter.
+
+s = "ABCDEFGHI"
+
+print(s[0:-2])
+
+print(s[:])
+
+print(s[-5:7])
+
+print(s[-2:-1])
