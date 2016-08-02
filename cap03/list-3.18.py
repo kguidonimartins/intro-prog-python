e01b2f9af59fb8104b6b128312974005e183b447
Karlo Guidoni Martins <karlogregorio@gmail.com>
2016-08-02 20:20:51 -0300

    exemplos com a função input

---
+++ cap03/list-3.18.py
@@ -0,0 +1,8 @@
+# Nota: a função 'input' funcionou no SublimeREPL, mas é preciso digitar
+#linha por linha.
+
+# Exemplo entrada de dados.
+
+nome = input("Digite seu nome: ")
+print("Você digitou %s" % nome)
+print("Olá, %s!" % nome)
---
+++ cap03/list-3.20.py
@@ -0,0 +1,6 @@
+# Exemplos de entrada de dados com números inteiros ou decimais
+
+anos = int(input("Anos de serviço: "))
+valor_por_ano = float(input("Valor por ano: "))
+bônus = anos * valor_por_ano
+print("Bônus de R$ %5.2f" % bônus)
